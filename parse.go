0648ded7550f459fba567771b70a076280e7be79
SemihBKGR <buyukgungorsemih@gmail.com>
2022-06-24 23:29:00 +0300

    parse diff result

--- parse.go
+++ parse.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 type Diff struct {
 	StartCommit  string
 	EndCommit    string
@@ -18,5 +20,41 @@ type DiffLine struct {
 }
 
 func parseDiff(s string) (*Diff, error) {
-	return nil, nil
+	diff := Diff{}
+	changeFiles := strings.Split(s, "diff --git")
+	diff.ChangedFiles = make([]DiffFile, 0, len(changeFiles))
+	for _, changeFile := range changeFiles {
+		diffFile := DiffFile{}
+		changes := strings.Split(changeFile, "@@")
+
+		fileMetadata := changes[0]
+		filename := parseMetadata(fileMetadata)
+		diffFile.Filename = filename
+
+		diffFile.ChangesLines = make([]DiffLine, 0, len(changes)-1)
+		for i := 1; i < len(changes); i++ {
+			lines := strings.Split(changes[i], "\n")
+			//todo: calculate line number
+			for _, line := range lines {
+				if len(line) > 0 && (line[0] == '+' || line[0] == '-') {
+					diffLine := DiffLine{
+						Content: line[1:],
+						IsAdded: line[0] == '+',
+					}
+					diffFile.ChangesLines = append(diffFile.ChangesLines, diffLine)
+				}
+			}
+		}
+		diff.ChangedFiles = append(diff.ChangedFiles, diffFile)
+	}
+	return &diff, nil
+}
+
+func parseMetadata(m string) string {
+	for _, l := range strings.Split(m, "\n") {
+		if strings.HasPrefix(l, "+++") {
+			return l[7:]
+		}
+	}
+	return ""
 }
